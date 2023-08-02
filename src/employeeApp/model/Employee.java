c6e0a314a9f32bf38e8170e2c0b2d6c2054faf3e
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-08-02 14:49:18 +0300

    Employee created - setter&getter method

---
+++ src/employeeApp/model/Employee.java
@@ -0,0 +1,68 @@
+package employeeApp.model;
+
+import java.util.Arrays;
+
+public class Employee {
+
+    private long id;
+    private String fullname;
+    private String email;
+    private String password;
+    private String[] healthPlans;
+
+
+    //GETTER METHODS
+    public  long getId(){
+        return id;
+    }
+
+    public String getFullName(){
+        return fullname;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String[] getHealthPlans(){
+        return healthPlans;
+    }
+
+    //SETTER METHODS
+
+    public void setId(){
+        this.id=id;
+    }
+    public void setFullName(){
+        this.fullname=fullname;
+    }
+    public void setEmail(){
+        this.email=email;
+    }
+    public void setPassword(){
+        this.password=password;
+    }
+    public void setHealthPlans(){
+        this.healthPlans = healthPlans;
+    }
+
+    public void addHealthPlan(int index, String name){
+        try{
+            if (healthPlans == null){
+                healthPlans[index] = name;
+            }else{
+                System.out.println("İlgili index dolu..");
+            }
+        }catch(ArrayIndexOutOfBoundsException ex) {//alacağımız hatayı ön gördük
+            System.out.println("olmayan index için atama yapıldı");//indexin lengthinden büyük bir endekse name girmeye çalıştık
+        }
+    }
+
+    public String toString(){
+        return "FullName: "+fullname+"Healthplan: "+ Arrays.toString(healthPlans);
+    }
+}
